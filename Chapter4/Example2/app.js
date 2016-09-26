cdc60e91e34b9aa6bb5da9651b08e94f971b6cf7
saralip <saralip@csu.fullerton.edu>
2016-09-25 22:10:35 -0700

    Made Comment App into a Chat App

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,27 +1,78 @@
-var main = function () {
+var main = function () 
+{
     "use strict";
 
-    var addCommentFromInputBox = function () {
-        var $new_comment;
+    var inputElements = 
+    [ $(".comment-input .user1"),
+    $(".comment-input .user2 "),
+    $(".comment-input .user1 button"),
+    $(".comment-input .user2 button")
+    ];
+    
 
-        if ($(".comment-input input").val() !== "") {
-            $new_comment = $("<p>").text($(".comment-input input").val());
+    var addCommentFromInputBox = function () 
+    {
+        var $new_comment;
+        var backgroundcolor;
+        var $whichUser;
+
+        if (arguments[0] == 1)
+        {
+            $whichUser=$(".comment-input .user1 input");
+            backgroundcolor="#ffccff";
+
+        }
+        else
+        {
+            $whichUser=$(".comment-input .user2 input");
+            backgroundcolor="#ccff99";
+        }
+        if (($whichUser).val() !== "")
+        {
+            $new_comment = $("<p>").text(($whichUser).val());
             $new_comment.hide();
             $(".comments").append($new_comment);
             $new_comment.fadeIn();
-            $(".comment-input input").val("");
+            $(".comments p:last-child").css("background-color", backgroundcolor);
+            ($whichUser).val("");
+
         }
     };
 
-    $(".comment-input button").on("click", function (event) {
-        addCommentFromInputBox();
-    });
+    var $i = $(".comment-input button").toArray();
+    ($i).forEach(function (element)
+    {
+        $(element).on("click", function ()
+        {
+    if ((element) === ($i[0]))
+    {
+        addCommentFromInputBox(1);
+        console.log("user 1 clicked");
+    }
+    else
+    {         
+        addCommentFromInputBox(2);
+        console.log("user 2 clicked");
+    }
+    
+    })});
 
-    $(".comment-input input").on("keypress", function (event) {
+    $(".comment-input .user1 ").on("keypress", function (event)
+    {
         if (event.keyCode === 13) {
-            addCommentFromInputBox();
+            addCommentFromInputBox(1);
         }
     });
+
+    $(".comment-input .user2 ").on("keypress", function (event)
+     {
+        if (event.keyCode === 13)
+        {
+            addCommentFromInputBox(2);
+      }
+    });
+
+
 };
 
 $(document).ready(main);
